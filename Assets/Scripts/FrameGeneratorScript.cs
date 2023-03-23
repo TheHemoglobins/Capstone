2a5494cc528db7922002816209b2221eaad6d129
Katie <katie.rosasco1@gmail.com>
2023-03-22 22:02:23 -0500

    initial changes implemented as talked about

--- Assets/Scripts/FrameGeneratorScript.cs
+++ Assets/Scripts/FrameGeneratorScript.cs
@@ -5,11 +5,13 @@ using System.Linq;
 
 public class FrameGeneratorScript : MonoBehaviour
 {
-    public GameObject prefabFrame;
+    //Struct detailing the rectangle for each hiddenWall
+    struct anchors(){
+        cornerR: Corner,
+        cornerL: Corner
+    }
 
-    public Vector3 minGroundPos;
-    public Vector3 maxGroundPos;
-
+    public GameObject frameTemplate;
     public List<GameObject> frameList = new List<GameObject>();
 
     [SerializeField]
@@ -18,7 +20,9 @@ public class FrameGeneratorScript : MonoBehaviour
     public int distanceBetween;
     [SerializeField]
     public float SnapDistance = 2f;
-    public Transform[] anchors;
+
+    //Input for hidden gameObjects next to walls
+    public Transform[] hiddenWalls;
 
     GameObject newFrame;
     float distance;
@@ -26,20 +30,42 @@ public class FrameGeneratorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        newFrame = Instantiate(prefabFrame, new Vector3(71, 1, 62), Quaternion.identity);
+        //Will we want the middle vector to be an input situation? Possibly a list or array
+        //What is a quaterion
+        //could we make it so that this loops through in following way:
+        //      hiddenWalls = array (make a hidden game object rectangle that is the size of a wall and skinny)
+        //      anchors = array (matches up to each game object above)
+        //      for i in length prefabframe{
+        //          new frame = instantiate(hiddenWalls[i], anchors[i], quaterions[i])
+        //          frameList.Add(newFrame)
+        //      }
 
-        frameList.Add(newFrame);
+        //Grabs the bottom right corner and top left corner of each wall in hiddenWalls
+        foreach (GameObject wall in hiddenWalls){
+            anchors(getCorner('right', wall), getCorner('left', wall));
+        }
+
+        //Creates a new frame for each hiddenWall 
+        for (i = 0; i < hiddenWalls; i++){
+            //Instantiate parameters = (Template, position, rotation)
+            newFrame = Instantiate(hiddenWalls[i], anchors[i], Quaternion.identity);
+            frameList.Add(newFrame);
+        }
     }
 
-    void Update()
+    void Generate()
     {
+        //we will be redoing this in a less confusing way but good start
+        //get rid of frameList.Count
         for (int i = 0; i < frameList.Count && frameList.Count != numRunTry; i++){
             Vector3 randomSpawnPos = new Vector3(
+                //possibly use this instead of the prefab frames (or maybe with) to squash to walls
                 Random.Range(minGroundPos.x, maxGroundPos.x),
                 1,
                 Random.Range(minGroundPos.z, maxGroundPos.z)
             );
 
+            //break these into two different for loops, have an array or list of randomSpawnPositions that are in same order as frames.
             foreach (GameObject frame in frameList)
             {
                 distance = Mathf.Sqrt( Mathf.Pow( (frame.transform.position.x - randomSpawnPos.x), 2) + Mathf.Pow( (frame.transform.position.z - randomSpawnPos.z), 2));
@@ -49,7 +75,7 @@ public class FrameGeneratorScript : MonoBehaviour
                 }
 
                 else if(distance >= distanceBetween && frame == frameList.Last()){
-                    newFrame = Instantiate(prefabFrame, randomSpawnPos, Quaternion.identity);
+                    newFrame = Instantiate(frameTemplate, randomSpawnPos, Quaternion.identity);
                     frameList.Add(newFrame);
                     //SnapOn(newFrame, randomSpawnPos);
 
@@ -59,7 +85,7 @@ public class FrameGeneratorScript : MonoBehaviour
         }
     }
 
-    void SnapOn(GameObject frame, Vector3 spawnPosition){
+    private void SnapOn(GameObject frame, Vector3 spawnPosition){
         Vector3 point = Vector3.zero;
         Vector3 pos;
         foreach (var anchor in anchors)
@@ -70,24 +96,34 @@ public class FrameGeneratorScript : MonoBehaviour
             distance = d;
             point = anchor.transform.position;
             }
-            //pos = new Vector3 (Random.Range(point.x * anchor.localScale.x, point.x), point.y, Random.Range(point.z * anchor.localScale.x, point.z));
-            if (anchor.transform.eulerAngles.x > 0f && anchor.transform.eulerAngles.x < 360){
-                pos = new Vector3(Random.Range(point.x - 4, point.x), point.y, Random.Range(point.z -4, point.z));
-            }else if (anchor.transform.eulerAngles.y > 0f && anchor.transform.eulerAngles.y < 360){
-                pos = new Vector3(Random.Range(point.z -4, point.z), point.y, Random.Range(point.x - 4, point.x));
-            }else if (anchor.transform.eulerAngles.z > 0f && anchor.transform.eulerAngles.z < 360){
-                pos = new Vector3(Random.Range(point.z -4, point.z), point.y, Random.Range(point.x - 4, point.x));
-            }else{
-                pos = point;
-            }
+            pos = new Vector3 (Random.Range(point.x * anchor.localScale.x, point.x), point.y, Random.Range(point.z * anchor.localScale.x, point.z));
 
-            if (distance > SnapDistance){ // position is close enough to snap point put it there.
-                frame.transform.position = spawnPosition;
-            }
-            else{
-                frame.transform.position = pos;
-            }
+            frame.transform.position = distance > Snapdistance ?? spawnPosition : pos;
 
         }        
+    }
+
+    //add more private functions to make this legible code. Bc currently this is a bit of a yikes.
+
+    private Vector3 getCorner(String Corner, GameObject wall){
+        var x, z;
+        var y = wall.Transform.position.y;
+        switch (corner){
+            case corner == 'right':
+                //dummy variables until testing using this link as reference: 
+                //https://stackoverflow.com/questions/22605683/unity3d-how-to-determine-the-corners-of-a-gameobject-in-order-to-position-other
+                // Bottom right corner of the wall needed
+                x = wall.renderer.bound.max.x;
+                z = wall.renderer.bound.min.z;
+                return Vector3(x, y, z);
+                break;
+            case corner == 'left':
+                //dummy variables until testing
+                //Top Left corner of the wall needed
+                x = wall.renderer.bound.min.x;
+                z = wall.renderer.bound.max.z;
+                return Vector3(x, y, z);
+                break;
+        }
     }
 }
