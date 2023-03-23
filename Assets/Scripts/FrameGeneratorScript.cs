e33ef9283c154ebe470b39f93f9593fa42573b3a
Katie <katie.rosasco1@gmail.com>
2023-03-23 00:01:46 -0500

    done for the night

--- Assets/Scripts/FrameGeneratorScript.cs
+++ Assets/Scripts/FrameGeneratorScript.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 
 //Class detailing the rectangle for each hiddenWall
-public class anchors{
+public class Anchors{
     public Vector3 cornerR;
     public Vector3 cornerL;
     public Vector3 corners(Vector3 cornerR, Vector3 cornerL){
@@ -52,58 +52,68 @@ public class FrameGeneratorScript : MonoBehaviour{
     public Transform[] hiddenWalls;
     private List<Vector3> anchorList;
 
+    private Anchors wallAnchors;
+
     GameObject newFrame;
     float distance;
 
     // Start is called before the first frame update
     void Start(){
-/*      Will we want the middle vector to be an input situation? Possibly a list or array
-        What is a quaterion
-        could we make it so that this loops through in following way:
-             hiddenWalls = array (make a hidden game object rectangle that is the size of a wall and skinny)
-             anchors = array (matches up to each game object above)
-             for i in length prefabframe{
-                 new frame = instantiate(hiddenWalls[i], anchors[i], quaterions[i])
-                 frameList.Add(newFrame)
-             } */
 
         //Grabs the bottom right corner and top left corner of each wall in hiddenWalls
         foreach (GameObject wall in hiddenWalls){
-            anchorList.Add(anchors(getCorner('right', wall), getCorner('left', wall)));
+            wallAnchors = new Anchor();
+            anchorList.Add(wallAnchors(getCorner('right', wall), getCorner('left', wall)));
         }
 
         //Creates a new frame for each hiddenWall 
         for (i = 0; i < hiddenWalls; i++){
             //Instantiate parameters = (Template, position, rotation)
+            //Snapping should be taken care of in the actual generation of the frame position
             newFrame = Instantiate(frameTemplate, generateFramePos(anchorList[i]), Quaternion.identity);
             frameList.Add(newFrame);
-            //Snap frame to wall
-            SnapOn(newFrame);
+
+            //Check distances between frames
+            distance = Mathf.Sqrt( Mathf.Pow( (newFrame[i].transform.position.x - newFrame[i-1].x), 2) + Mathf.Pow( (newFrame[i].transform.position.z - newFrame[i-1].z), 2));
+
+            if (distance > distanceBetween){
+                //I dont know what would go here
+                break;
+            }
+
+            //Check Rotation of frame in accordance with the wall
+            wall = hiddenWalls[i];
 
         }
     }
 
     //getRotation for seeing if the frame is in the correct rotation, can also be used with walls
     private Vector3 getRotation(GameObject frame){
-        frame.eulerAngles.x = frame.eulerAngles.x <= 180f ?? frame.eulerAngles.x : frame.eulerAngles.x -360f;
-        frame.eulerAngles.y = frame.eulerAngles.y <= 180f ?? frame.eulerAngles.y : frame.eulerAngles.y -360f; 
-        frame.eulerAngles.z = frame.eulerAngles.z <= 180f ?? frame.eulerAngles.z : frame.eulerAngles.z -360f; 
-        return Vector3(frame.eulerAngles.x, frame.eulerAngles.y, frame.eulerAngler.z); 
+        rotation = frame.eulerAngles;
+
+        rotation.x = rotation.x <= 180f ?? rotation.x : rotation.x -360f;
+        rotation.y = rotation.y <= 180f ?? rotation.y : rotation.y -360f; 
+        rotation.z = rotation.z <= 180f ?? rotation.z : rotation.z -360f; 
+        return overallRotation = new Vector3(rotation.x, rotation.y, rotation.z); 
     }
 
-    private Vector3 generateFramePos(Vector3 anchor){
-        //Create a random position withing the anchor's range
-        return randomSpawPos = new Vetor3(
+    private Vector3 generateFramePos(Anchors anchor){
+        //Create a random position within the anchor's range should take care of snapping
+        return randomPosition = new Vector3(
             Random.Range(anchor.cornerR.x, anchor.cornerL.x),
             Random.Range(anchor.cornerR.z), Random.Range(anchor.cornerL.z),
             wall.transform.position.y
         )
     }
 }
+
+//I dont think we will need anything under this
+//======================================================================
+
         //we will be redoing this in a less confusing way but good start
         //get rid of frameList.Count
         /* for (int i = 0; i < frameList.Count && frameList.Count != numRunTry; i++){
-            Vector3 randomSpawnPos = new Vector3(
+            Vector3 newFrame[i-1] = new Vector3(
                 //possibly use this instead of the prefab frames (or maybe with) to squash to walls
                 Random.Range(minGroundPos.x, maxGroundPos.x),
                 1,
@@ -113,14 +123,14 @@ public class FrameGeneratorScript : MonoBehaviour{
             //break these into two different for loops, have an array or list of randomSpawnPositions that are in same order as frames.
             foreach (GameObject frame in frameList)
             {
-                distance = Mathf.Sqrt( Mathf.Pow( (frame.transform.position.x - randomSpawnPos.x), 2) + Mathf.Pow( (frame.transform.position.z - randomSpawnPos.z), 2));
+                distance = Mathf.Sqrt( Mathf.Pow( (frame.transform.position.x - newFrame[i-1].x), 2) + Mathf.Pow( (frame.transform.position.z - newFrame[i-1].z), 2));
 
                 if (distance < distanceBetween){
                     break;
                 }
 
                 else if(distance >= distanceBetween && frame == frameList.Last()){
-                    newFrame = Instantiate(frameTemplate, randomSpawnPos, Quaternion.identity);
+                    newFrame = Instantiate(frameTemplate, newFrame[i-1], Quaternion.identity);
                     frameList.Add(newFrame);
 
                     break;
@@ -145,6 +155,3 @@ public class FrameGeneratorScript : MonoBehaviour{
             frame.transform.position = distance > Snapdistance ?? spawnPosition : pos; 
 
         }*/
-
-    //add more private functions to make this legible code. Bc currently this is a bit of a yikes.
-
