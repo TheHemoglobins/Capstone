526d29a57f17c19a2c99579e520e82cd4833a8ac
Katie <katie.rosasco1@gmail.com>
2023-04-05 16:12:00 -0500

    FileBrowser working

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using SimpleFileBrowser;
-using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 
@@ -9,8 +8,11 @@ public class ButtonHandler : MonoBehaviour
 {
     private List<string> uploadedPhotoPaths = new List<string>();
     private VisualElement frame;
-    private GroupBox column;
+    private GroupBox column1;
+    private GroupBox column2;
     private Button button;
+    private Label label;
+    private string newText;
 
     private void Start() {
         FileBrowser.SetFilters(true, new FileBrowser.Filter( "Images", ".jpg", ".png" ));
@@ -22,15 +24,35 @@ public class ButtonHandler : MonoBehaviour
         var rootVisualElement = uiDocument.rootVisualElement;
  
         this.frame = rootVisualElement.Q<VisualElement>("Canvas");
-        this.column = frame.Q<GroupBox>("FrontMatterColumn");
-        this.button = column.Q<Button>("UploadButton");
+        this.column1 = frame.Q<GroupBox>("FrontMatterColumn");
+        this.button = column1.Q<Button>("UploadButton");
+
+        this.column2 = frame.Q<GroupBox>("UploadedPhotosColumns");
+        this.label = column2.Q<Label>("UploadedPhotoTitle");
  
-        this.button.RegisterCallback<ClickEvent>(ev => openFileBrowser());
+        this.button.RegisterCallback<ClickEvent>(ev => {
+            FileBrowser.ShowLoadDialog( ( uploadedPhotoPaths ) => { Debug.Log( "Selected: " + uploadedPhotoPaths[0] ); },
+								   () => { Debug.Log( "Canceled" ); },
+								   FileBrowser.PickMode.Files, true, null, null, "Select Files", "Select" );
+        });
+    }
+
+    private void Update() {
+        for(int i = 0; i < FileBrowser.Result.Length; i++){
+            this.newText = uploadedPhotoPaths[i];
+            Debug.Log(newText);
+            setText();
+        }
+    }
+
+
+    public void setText(){
+        this.label.text = this.newText;
     }
  
-    IEnumerator openFileBrowser()
+    IEnumerator openFileBrowserCoroutine()
     {
-        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.FilesAndFolders, true, null, null, "Load Files and Folders", "Load");
+        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Files, true, null, null, "Load Files", "Select");
         if(FileBrowser.Success)
 		{
             for(int i = 0; i < FileBrowser.Result.Length; i++){
