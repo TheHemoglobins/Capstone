4f68a2701159f5d4da4c21e502d34a2b88ed951b
Katie <katie.rosasco1@gmail.com>
2023-04-05 17:56:16 -0500

    Updating photo titles

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -6,13 +6,12 @@ using System.Collections;
 
 public class ButtonHandler : MonoBehaviour
 {
-    private List<string> uploadedPhotoPaths = new List<string>();
+    private string[] uploadedPhotoPaths;
     private VisualElement frame;
     private GroupBox column1;
     private GroupBox column2;
     private Button button;
     private Label label;
-    private string newText;
 
     private void Start() {
         FileBrowser.SetFilters(true, new FileBrowser.Filter( "Images", ".jpg", ".png" ));
@@ -23,33 +22,50 @@ public class ButtonHandler : MonoBehaviour
         var uiDocument = GetComponent<UIDocument>();
         var rootVisualElement = uiDocument.rootVisualElement;
  
+        List<VisualElement> labelList = GetListOfLabels(rootVisualElement);
+      
+        this.button.RegisterCallback<ClickEvent>(ev => {
+            FileBrowser.ShowLoadDialog(
+                (newPhotoPaths) => {OnSuccess(newPhotoPaths, labelList);},
+				() => { Debug.Log( "Canceled" ); },
+				FileBrowser.PickMode.Files, true, null, null, "Select Files", "Select" );
+        });
+    }
+/* 
+    private void Update() {
+        for(int i = 0; i < this.uploadedPhotoPaths.Length; i++){
+            this.newText = this.uploadedPhotoPaths[i];
+            Debug.Log(newText);
+            setText();
+        }
+    }
+ */
+    public List<VisualElement> GetListOfLabels(VisualElement rootVisualElement){
+
         this.frame = rootVisualElement.Q<VisualElement>("Canvas");
         this.column1 = frame.Q<GroupBox>("FrontMatterColumn");
         this.button = column1.Q<Button>("UploadButton");
 
         this.column2 = frame.Q<GroupBox>("UploadedPhotosColumns");
-        this.label = column2.Q<Label>("UploadedPhotoTitle");
- 
-        this.button.RegisterCallback<ClickEvent>(ev => {
-            FileBrowser.ShowLoadDialog( ( uploadedPhotoPaths ) => { Debug.Log( "Selected: " + uploadedPhotoPaths[0] ); },
-								   () => { Debug.Log( "Canceled" ); },
-								   FileBrowser.PickMode.Files, true, null, null, "Select Files", "Select" );
-        });
+
+        List<VisualElement> labelList = column2.Query(className: "photoTitle").ToList();
+
+        return labelList;
     }
 
-    private void Update() {
-        for(int i = 0; i < FileBrowser.Result.Length; i++){
-            this.newText = uploadedPhotoPaths[i];
-            Debug.Log(newText);
-            setText();
-        }
+    public void OnSuccess(string[] paths, List<VisualElement> labelList){
+        this.uploadedPhotoPaths = paths; 
+        for(var i = 0; i < this.uploadedPhotoPaths.Length; i++){
+            setText(labelList[i], uploadedPhotoPaths[i]);
+        };
     }
 
-
-    public void setText(){
-        this.label.text = this.newText;
+    public void setText(VisualElement uploadLabel, string photoPath){
+        this.label = uploadLabel as Label;
+        var splicedPhotoPath = photoPath;
+        this.label.text = $"Uploaded: {splicedPhotoPath}";
     }
- 
+/*  
     IEnumerator openFileBrowserCoroutine()
     {
         yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Files, true, null, null, "Load Files", "Select");
@@ -60,5 +76,5 @@ public class ButtonHandler : MonoBehaviour
             }
 		};
 
-    }
+    } */
 }
