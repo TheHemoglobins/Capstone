05b9709980b48710cd7e52440bc68504955cdd37
Katie <katie.rosasco1@gmail.com>
2023-04-06 11:09:56 -0500

    Dynamically generated labels and pagination, still some issues

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -3,6 +3,7 @@ using UnityEngine.UIElements;
 using SimpleFileBrowser;
 using System.Collections.Generic;
 using System.Collections;
+using System.IO;
 
 public class ButtonHandler : MonoBehaviour
 {
@@ -22,59 +23,92 @@ public class ButtonHandler : MonoBehaviour
         var uiDocument = GetComponent<UIDocument>();
         var rootVisualElement = uiDocument.rootVisualElement;
  
-        List<VisualElement> labelList = GetListOfLabels(rootVisualElement);
+        GetLabel(rootVisualElement);
       
         this.button.RegisterCallback<ClickEvent>(ev => {
             FileBrowser.ShowLoadDialog(
-                (newPhotoPaths) => {OnSuccess(newPhotoPaths, labelList);},
+                (newPhotoPaths) => {OnSuccess(newPhotoPaths, rootVisualElement);},
 				() => { Debug.Log( "Canceled" ); },
 				FileBrowser.PickMode.Files, true, null, null, "Select Files", "Select" );
         });
     }
-/* 
-    private void Update() {
-        for(int i = 0; i < this.uploadedPhotoPaths.Length; i++){
-            this.newText = this.uploadedPhotoPaths[i];
-            Debug.Log(newText);
-            setText();
-        }
-    }
- */
-    public List<VisualElement> GetListOfLabels(VisualElement rootVisualElement){
 
-        this.frame = rootVisualElement.Q<VisualElement>("Canvas");
+    public GroupBox GeneratePaginationArrows(VisualElement root){
+        this.frame = root.Q<VisualElement>("Canvas");
+        this.column2 = frame.Q<GroupBox>("UploadedPhotosColumns");
+        
+        GroupBox pagination = new GroupBox();
+        VisualElement backArrow = new VisualElement();
+        
+        backArrow.AddToClassList("backArrow");
+
+        pagination.Add(backArrow);
+        
+        pagination.AddToClassList("pagination");
+
+        column2.Add(pagination);
+
+        return pagination;
+    }
+
+    public void GeneratePagination(GroupBox pag, int n){
+
+        Button pageButton = new Button();
+        pageButton.text = $"{n}";
+
+        //Issues changing the page number to reflect 1 versus the number of interations
+        if(n > 0){
+            pageButton.text = $"{n-3}";
+        };
+
+        pag.Add(pageButton);
+
+    }
+
+    public VisualElement GetLabel(VisualElement root){
+
+        this.frame = root.Q<VisualElement>("Canvas");
         this.column1 = frame.Q<GroupBox>("FrontMatterColumn");
         this.button = column1.Q<Button>("UploadButton");
 
         this.column2 = frame.Q<GroupBox>("UploadedPhotosColumns");
 
-        List<VisualElement> labelList = column2.Query(className: "photoTitle").ToList();
+        Label label = new Label();
+        label.AddToClassList("photoTitle");
+        column2.Add(label);
 
-        return labelList;
+        return label as VisualElement;
     }
 
-    public void OnSuccess(string[] paths, List<VisualElement> labelList){
-        this.uploadedPhotoPaths = paths; 
-        for(var i = 0; i < this.uploadedPhotoPaths.Length; i++){
-            setText(labelList[i], uploadedPhotoPaths[i]);
+    public void OnSuccess(string[] paths, VisualElement root){
+        this.uploadedPhotoPaths = paths;
+
+        List<VisualElement> labelList = new List<VisualElement>();
+
+        GroupBox pagination = GeneratePaginationArrows(root);
+
+        for(int i = 0; i < paths.Length; i++){
+            VisualElement label = GetLabel(root);
+            labelList.Add(label);
+            SetUploadPhotoColumn(labelList[i], uploadedPhotoPaths[i]);
+            if((i % 3) == 0){
+                //Pagination is being generated before the labels thus placing it as the first element
+                //unknown as to why this is being generated first
+                GeneratePagination(pagination, i);
+            }
         };
+        VisualElement forwardArrow = new VisualElement();
+        //USS is drawing the wrong background image probably because of fileID/guid issues
+        forwardArrow.AddToClassList("forwardArrow");
+        pagination.Add(forwardArrow);
     }
 
-    public void setText(VisualElement uploadLabel, string photoPath){
+    public void SetUploadPhotoColumn(VisualElement uploadLabel, string photoPath){
         this.label = uploadLabel as Label;
-        var splicedPhotoPath = photoPath;
+
+        var splicedPhotoPath = Path.GetFileName(photoPath);
         this.label.text = $"Uploaded: {splicedPhotoPath}";
     }
-/*  
-    IEnumerator openFileBrowserCoroutine()
-    {
-        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Files, true, null, null, "Load Files", "Select");
-        if(FileBrowser.Success)
-		{
-            for(int i = 0; i < FileBrowser.Result.Length; i++){
-                this.uploadedPhotoPaths.Add(FileBrowser.Result[i]);
-            }
-		};
-
-    } */
 }
+
+//Stretch Goal: display photo previews
